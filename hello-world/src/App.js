6637fcf732876d613366b9f16709b3796774bea4
bavshehata <bavshehata@gmail.com>
2021-04-25 23:48:44 +0200

    ES6 arrow functions

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.svg'
 import Greet from './components/Greet.js'
 import './App.css';
 
--- hello-world/src/components/Greet.js
+++ hello-world/src/components/Greet.js
@@ -1,5 +1,3 @@
 import React from 'react';
-function Greet(){
-    return <h1>Hello There</h1>
-}
+const Greet = ()=> <h1>Hello There</h1>
 export default Greet;
