20fbd1065f1d2806dc5a96d24d2c5cc8f38b180d
bavshehata <bavshehata@gmail.com>
2021-04-26 13:39:14 +0200

    states in class components

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -2,16 +2,18 @@ import logo from "./logo.svg";
 import Greet from "./components/Greet.js";
 import Welcome from "./components/Welcome.js";
 import Hello from "./components/Hello.js";
+import Message from "./components/Message.js";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Bavly">
+      {/* <Greet name="Bavly">
         <p>I am a child</p>
       </Greet>
       <Welcome name="Jim" />
-      <Hello />
+      <Hello /> */}
+      <Message />
     </div>
   );
 }
