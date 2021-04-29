b73ccb90c1f285d20af02b86ab5b320fb07cefd7
bavshehata <bavshehata@gmail.com>
2021-04-29 23:58:29 +0200

    memo

---
+++ hello-world/src/components/MemoComp.js
@@ -0,0 +1,8 @@
+import React, { memo } from "react";
+
+function MemoComp({ name }) {
+  console.log("Rendering Memo Comp");
+  return <div>{name}</div>;
+}
+
+export default memo(MemoComp);
--- hello-world/src/components/ParentComp.js
+++ hello-world/src/components/ParentComp.js
@@ -1,8 +1,9 @@
 import React, { Component, PureComponent } from "react";
+import MemoComp from "./MemoComp";
 import PureComp from "./PureComp";
 import RegComp from "./RegComp";
 
-export class ParentComp extends PureComponent {
+export class ParentComp extends Component {
   constructor(props) {
     super(props);
 
@@ -18,8 +19,9 @@ export class ParentComp extends PureComponent {
     return (
       <div>
         Parent Component
-        <RegComp name={this.state.name} />
-        <PureComp name={this.state.name} />
+        <MemoComp name={this.state.name} />
+        {/* <RegComp name={this.state.name} />
+        <PureComp name={this.state.name} /> */}
       </div>
     );
   }
