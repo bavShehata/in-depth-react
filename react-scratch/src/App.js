3dae684ef4d662df18071604795df7a83cdbcd9a
bavshehata <bavshehata@gmail.com>
2021-05-02 20:34:34 +0200

    Building a react app from scratch

---
+++ react-scratch/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "App.css";
+import image from "./SE.jpg";
+const App = () => (
+  <div>
+    <h1>My react app</h1>
+    <img src={image} alt="cool image" />
+  </div>
+);
+
+export default App;
